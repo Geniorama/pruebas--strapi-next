75b88fb831857a80874c063b2b7226cfa6e35d05
AngelDevelop <angelpublicista.1@gmail.com>
2022-02-04 17:16:09 -0500

    first commit

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '921abb9478f194b41f0360060f546206'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'eb8b4a01-7cdc-4561-b349-dc7790886767'
+};
